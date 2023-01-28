f5dab0c360d74f5f7d7663a178ff4970a95c1318
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2023-01-27 18:06:28 -0700

    got basic layout

--- todo-svelte/src/App.svelte
+++ todo-svelte/src/App.svelte
@@ -1,11 +1,22 @@
 <script>
-
+  let todoItems = []
+  let todoInput = ''
+  function handleClick(todoInput){
+    todoItems.push(todoInput)
+  }
 </script>
 
 <main>
-  <h1>Todo List</h1><br>
-  <input type='text' id='todoInput' name='todoInput'/>
-  
+  <div>
+    <h1>Todo List</h1><br>
+    <input  bind:value= {todoInput}>
+    <button  on:click={handleClick}>Add Todo</button>
+    <ul> 
+      {#each todoItems as item}
+        <li><input type='checkbox'> &nbsp {item}</li>
+      {/each} 
+    </ul> 
+  </div>
 </main>
 
 <style>
